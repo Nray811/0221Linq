e8f2eab8f405b3932e520ee4e826a82f561fc005
Nray <ray08118686@gmail.com>
2023-02-21 11:46:15 +0800

    加入專案檔案。

---
+++ 0221/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0221_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("輸入一串以逗號分隔的字串，並將其反向顯示");
+            string s = Console.ReadLine();          
+            var n = s.Split(',');
+            var even = n.Where( x => int.Parse(x) % 2 == 0).OrderBy(x => int.Parse(x));
+            var odd = n.Where(  x => int.Parse(x) % 2 == 1).OrderBy(x => int.Parse(x));             
+            Console.WriteLine($"奇數.Orderby結果:{string.Join(",", odd )}");                     
+            Console.WriteLine($"偶數.Orderby結果:{string.Join(",", even)}");        
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ 0221_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0221_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("輸入一串以逗號分隔的字串，並將其反向顯示");
+            string reverse = Console.ReadLine();
+            var result = reverse.Split(',').Reverse();
+            Console.WriteLine($"Reverse結果:{string.Join(",", result)}");
+            Console.ReadKey();
+        }
+    }
+}
